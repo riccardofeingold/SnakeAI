fc7fbf2e5f3689d3375b10d18365330c2886f1db
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2024-03-30 10:07:32 +0100

    __init__.py files

---
+++ snake_game/__init__.py
@@ -0,0 +1,5 @@
+from .game import SnakeGame
+from .game_ai import SnakeGameAI
+from .objects import Snake, Food, GameObject
+from .ui import ScoreText
+from .utils import Constants, Colors, Directions, Point
---
+++ snake_game/objects/__init__.py
@@ -0,0 +1,3 @@
+from .food import Food
+from .game_object import GameObject
+from .snake import Snake
