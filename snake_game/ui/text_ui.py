3d260436a7bf36e0bd6b35b6d77184d8e326ec7a
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2024-03-30 10:09:15 +0100

    currently only has a simple score text display

---
+++ snake_game/ui/text_ui.py
@@ -0,0 +1,11 @@
+import pygame
+from snake_game.utils import Colors
+
+class ScoreText:
+    def __init__(self, display: pygame.display) -> None:
+        self.display = display
+        self.font = pygame.font.Font(None, 40)
+
+    def draw(self, score: int):
+        text = self.font.render("Score: " + str(score), True, Colors.WHITE.value)
+        self.display.blit(text, [0, 0])
