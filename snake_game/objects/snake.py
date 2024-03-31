2613160fc74bfe911db4e7406529d04e3823cb7b
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2024-04-01 00:18:57 +0200

    adjusted snake class such that NN can control it

--- snake_game/objects/snake.py
+++ snake_game/objects/snake.py
@@ -8,8 +8,8 @@ class Snake(GameObject):
     def __init__(self, start_position: Tuple[float, float], init_direction: Directions):
         self.head = Point(start_position[0], start_position[1])
         self.tail = [self.head,
-                     Point(self.head.x - Constants.BLOCK_SIZE.value, self.head.y),
-                     Point(self.head.x - 2*Constants.BLOCK_SIZE.value, self.head.y)
+                     Point(self.head.x - Constants.BLOCK_SIZE, self.head.y),
+                     Point(self.head.x - 2*Constants.BLOCK_SIZE, self.head.y)
                     ]
         self.direction = init_direction
         self.color = Colors.BLUE1
@@ -18,25 +18,32 @@ class Snake(GameObject):
     def spawn(self):
         pass
 
-    def move(self):
+    def move(self, action):
+        # action = [straight, left, right]
+        clock_wise_directions = [Directions.UP, Directions.RIGHT, Directions.DOWN, Directions.LEFT]
+        if action[1]:
+            self.direction = clock_wise_directions[clock_wise_directions.index(self.direction) - 1]
+        elif action[2]:
+            self.direction = clock_wise_directions[(clock_wise_directions.index(self.direction) + 1)%len(clock_wise_directions)]
+
         new_head_x = self.head.x
         new_head_y = self.head.y
 
         if self.direction == Directions.RIGHT:
-            new_head_x += Constants.BLOCK_SIZE.value
+            new_head_x += Constants.BLOCK_SIZE
         elif self.direction == Directions.LEFT:
-            new_head_x -= Constants.BLOCK_SIZE.value
+            new_head_x -= Constants.BLOCK_SIZE
         elif self.direction == Directions.DOWN:
-            new_head_y += Constants.BLOCK_SIZE.value
+            new_head_y += Constants.BLOCK_SIZE
         elif self.direction == Directions.UP:
-            new_head_y -= Constants.BLOCK_SIZE.value
+            new_head_y -= Constants.BLOCK_SIZE
 
         self.head = Point(new_head_x, new_head_y)
 
     def draw(self, display):
         for piece in self.tail:
-            pygame.draw.rect(display, Colors.BLUE1.value, pygame.Rect(piece.x, piece.y, Constants.BLOCK_SIZE.value, Constants.BLOCK_SIZE.value))
-            pygame.draw.rect(display, Colors.BLUE2.value, pygame.Rect(piece.x+4, piece.y+4, 12, 12))
+            pygame.draw.rect(display, Colors.BLUE1, pygame.Rect(piece.x, piece.y, Constants.BLOCK_SIZE, Constants.BLOCK_SIZE))
+            pygame.draw.rect(display, Colors.BLUE2, pygame.Rect(piece.x+4, piece.y+4, 12, 12))
         pass
 
     def selfDestroy(self):
