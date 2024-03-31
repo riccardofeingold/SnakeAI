a456c5fe59f621e0acb9b350d68a7e1833fa5424
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2024-04-01 00:35:19 +0200

    main file where you can switch between train and play

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pygame
+import random
+from snake_game.utils import Directions
 from snake_game import SnakeGame
+from agent import train
+
 if __name__ == "__main__":
     pygame.init()
+    train()
 
-    game = SnakeGame()
-    while True:
-        game_over, score = game.step()
+    # game = SnakeGame()
+    # while True:
+    #     game_over, score = game.step()
 
-        if game_over:
-            break
+    #     if game_over:
+    #         break
     
-    pygame.quit()
+    # pygame.quit()
